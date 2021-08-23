f2e9668114be3e837a45d4bfb74e6bf4d38ba895
Francisco Yair Rodriguez Martinez <yairrdz11@gmail.com>
2021-08-23 15:10:19 -0500

    Initial Component linked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { InitComponent } from "./views/init/init.component";
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: InitComponent
+  }
 ]
 
 @NgModule({
